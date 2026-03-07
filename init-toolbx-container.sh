fdab633f8f384f0c03db16d2693a9f61cb887943
Olivier Serve Séchet <tifauv@gmail.com>
2026-03-07 15:23:53 +0100

    Add ascript to initialize a Toolbx container for building the game

---
+++ init-toolbx-container.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Name of the development container
+NAME=dev-rust
+
+# List of packages required in the container
+PKGS="cargo
+    wayland-devel
+    g++
+    alsa-lib-devel
+    libudev-devel
+    libxkbcommon"
+
+# Create the container only if it does not already exist
+exists=$(toolbox list --containers | awk '$2 == "dev-rust"' | wc -l)
+if [[ $exists -eq 0 ]]
+then # Create the toolbox container
+    toolbox create "${NAME}" --distro fedora --release 43
+fi
+
+# Update and initialize the container
+toolbox run -c "${NAME}" sudo dnf --assumeyes update
+toolbox run -c "${NAME}" sudo dnf --assumeyes install ${PKGS}
+
