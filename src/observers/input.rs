f6db26cfd84eb153c0e2e18076fe7b731b00fbca
Olivier Serve Séchet <tifauv@gmail.com>
2026-03-11 09:11:29 +0100

    input: Add missing functions (un)grab_cursor

--- src/observers/input.rs
+++ src/observers/input.rs
@@ -1,16 +1,15 @@
 use bevy::prelude::*;
-use bevy::window::CursorOptions;
+use bevy::window::{CursorGrabMode, CursorOptions};
 use bevy_enhanced_input::prelude::*;
 
 use crate::components::input_actions::*;
-use crate::resources::input::grab_cursor;
 
 
 pub fn capture_cursor(
     _on: On<Complete<CaptureCursor>>,
     mut p_cursor: Single<&mut CursorOptions>,
 ) {
-    grab_cursor(&mut p_cursor, true);
+    grab_cursor(&mut p_cursor);
 }
 
 
@@ -18,5 +17,17 @@ pub fn release_cursor(
     _on: On<Complete<ReleaseCursor>>,
     mut p_cursor: Single<&mut CursorOptions>,
 ) {
-    grab_cursor(&mut p_cursor, false);
+    ungrab_cursor(&mut p_cursor);
+}
+
+
+fn grab_cursor(p_cursor: &mut CursorOptions) {
+    p_cursor.grab_mode = CursorGrabMode::Confined;
+    p_cursor.visible = false;
+}
+
+
+fn ungrab_cursor(p_cursor: &mut CursorOptions) {
+    p_cursor.grab_mode = CursorGrabMode::None;
+    p_cursor.visible = true;
 }
