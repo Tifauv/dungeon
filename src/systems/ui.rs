6c917e8a48dd93a352c30c35303f9047d12f69df
Olivier Serve Séchet <tifauv@gmail.com>
2026-03-15 15:44:16 +0100

    system ui: function parameters follow the project's style
    All function parameters have a 'p_' prefix, to identify its scope.

--- src/systems/ui.rs
+++ src/systems/ui.rs
@@ -4,17 +4,17 @@ use bevy::window::WindowResized;
 use crate::components::base::*;
 
 pub fn set_camera_viewports(
-    windows: Query<&Window>,
-    mut window_resized_reader: MessageReader<WindowResized>,
-    mut query: Query<(&CameraView, &mut Camera)>,
+    p_windows: Query<&Window>,
+    mut p_window_resized_reader: MessageReader<WindowResized>,
+    mut p_query: Query<(&CameraView, &mut Camera)>,
 ) {
     // We need to dynamically resize the camera's viewports whenever the window size changes
     // so then each camera always takes up half the screen.
     // A resize_event is sent when the window is first created, allowing us to reuse this system for initial setup.
-    for window_resized in window_resized_reader.read() {
-        let window = windows.get(window_resized.window).unwrap();
+    for window_resized in p_window_resized_reader.read() {
+        let window = p_windows.get(window_resized.window).unwrap();
 
-        for (view, mut camera) in &mut query {
+        for (view, mut camera) in &mut p_query {
             let size = window.physical_size().as_vec2() * view.size;
             camera.viewport = Some(Viewport {
                 physical_position: view.pos * size.as_uvec2(),
